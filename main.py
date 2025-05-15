654e736277e8c93bb384388634d13f2f1e7851ed
tejak6958 <tejak6958@gmail.com>
2025-05-15 11:49:06 +0530

    Data Validation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.Data_ingestion import DataIngestion
+from networksecurity.components.Data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
-from networksecurity.entity.artifact_entity import DataIngestionArtifact
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 
 import sys
@@ -15,6 +16,12 @@ if __name__ == "__main__":
         data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
         logging.info(data_ingestion_artifact)
+        data_validation_config=DataValidationConfig(training_pipeline_config)
+        data_validation=DataValidation(data_ingestion_artifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -15,6 +15,7 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
 
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
 
 '''
 Data ingestion related constant start with data_ingestion
@@ -27,3 +28,12 @@ DATA_INGESTION_FEATURE_STORE_DIR = 'feature_store'
 DATA_INGESTION_INGESTED_DIR = 'ingested'
 DATA_INGESTION_INGESTED_TRAIN_TEST_SPLIT_RATIO = 0.2
 
+'''
+Data validation related constant start with data_validation
+'''
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
