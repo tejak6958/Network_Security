7041ecdee119f7aa2723c891f7ae44a7d0f6c881
tejak6958 <tejak6958@gmail.com>
2025-05-18 18:06:36 +0530

    Pipeline Completed

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from networksecurity.components.Data_ingestion import DataIngestion
 from networksecurity.components.Data_validation import DataValidation
+from networksecurity.components.Data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.artifact_entity import DataIngestionArtifact,DataValidationArtifact
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+ 
 
 import sys
 import os
@@ -22,6 +26,19 @@ if __name__ == "__main__":
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("data Validation Completed")
         print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        logging.info("data Transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data Transformation completed")
+
+        logging.info("Model Training sstared")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Model Training artifact created")
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
